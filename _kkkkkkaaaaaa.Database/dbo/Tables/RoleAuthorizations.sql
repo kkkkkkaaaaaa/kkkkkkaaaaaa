1bf625fd438041ae7773348254a4b1846d687793
Kanda, Yuichiro <kkkkkkaaaaaa@hotmail.co.jp>
2012-09-21 08:49:46 +0900

    Delete Database.2010 project.

--- _kkkkkkaaaaaa.Database/dbo/Tables/RoleAuthorizations.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[RoleAuthorizations] (
-    [RoleID]          BIGINT NOT NULL,
-    [AuthorizationID] BIGINT NOT NULL,
-    CONSTRAINT [PK_RoleAuthorizations_1] PRIMARY KEY CLUSTERED ([RoleID] ASC, [AuthorizationID] ASC)
-);
-
--- _kkkkkkaaaaaa.Database/dbo/Tables/UserRoles.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[UserRoles] (
-    [UserID] BIGINT NOT NULL,
-    [RoleID] BIGINT NOT NULL,
-    CONSTRAINT [PK_UserRoles] PRIMARY KEY CLUSTERED ([UserID] ASC, [RoleID] ASC)
-);
-

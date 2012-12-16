f94d355ec4231e277e9b0990563d3ae124351e46
Kanda, Yuichiro <kkkkkkaaaaaa@hotmail.co.jp>
2012-12-16 23:34:37 +0900

    Add kkkkkkaaaaaa.Runtime.InteropServices.

--- kkkkkkaaaaaa.Database/Tables/UserAttributeItems.sql
+++ kkkkkkaaaaaa.Database/Tables/UserAttributeItems.sql
@@ -2,7 +2,8 @@
     [ID]          INT           NOT NULL,
     [Name]        VARCHAR (64)  NOT NULL,
     [Description] VARCHAR (MAX) NOT NULL,
-    [Enabled]     BIT           NOT NULL, 
+    [Ordinal] INT NOT NULL,
+    [Enabled]     BIT           NOT NULL,
     CONSTRAINT [PK_UserAttributeItems] PRIMARY KEY ([ID])
 );
 

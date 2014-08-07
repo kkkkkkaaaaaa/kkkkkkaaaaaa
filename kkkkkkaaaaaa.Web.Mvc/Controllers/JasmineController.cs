4ffa5933903ebab0ca23c98c904901fad2dfb8c1
kkkkkkaaaaaa <kkkkkkaaaaaa@hotmail.co.jp>
2014-08-07 21:24:04 +0900

    Refer xUnit.NET from NuGet.

---
+++ kkkkkkaaaaaa.Web.Mvc/Controllers/JasmineController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.Mvc;
+
+namespace kkkkkkaaaaaa.Web.Mvc.Controllers
+{
+    public class JasmineController : Controller
+    {
+        public ViewResult Run()
+        {
+            return View("SpecRunner");
+        }
+    }
+}
---
+++ kkkkkkaaaaaa.Web.Mvc/Scripts/jasmine/spec/IndexSpec.ts
@@ -0,0 +1,19 @@
+﻿function add(a: number, b: number) {
+    return a + b;
+}
+
+describe("add", () => {
+    var doNothing = () => {};
+
+    beforeEach(() => {
+
+        doNothing();
+    });
+
+    it("", () => {
+        expect(add(1, 1))
+            .not
+            .toBe(2);
+
+    });
+});

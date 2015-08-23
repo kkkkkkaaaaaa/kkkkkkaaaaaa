8092cceff352d33ad31433e68a6d413b5317ee47
kkkkkkaaaaaa <kkkkkkaaaaaa@hotmail.co.jp>
2015-08-23 16:42:59 +0900

    Add asynchronous test.

---
+++ kkkkkkaaaaaa.kkkkkkaaaaaa.Xunit/Data/TableDataGateways/KandaTableDataGatewayFacts.cs
@@ -0,0 +1,29 @@
+﻿using kkkkkkaaaaaa.Data.Common;
+
+namespace kkkkkkaaaaaa.kkkkkkaaaaaa.Xunit.Data.TableDataGateways
+{
+    /// <summary>
+    /// TableDataGatewayFacts の基底クラスを表します。
+    /// </summary>
+    public class KandaTableDataGatewayFacts
+    {
+        #region Protected members...
+
+        /// <summary>
+        /// データベースプロバイダーのファクトリー。
+        /// </summary>
+        protected KandaProviderFactory Factory
+        {
+            get { return this._factory; }
+        }
+
+        #endregion
+
+        #region Private members...
+
+        /// <summary></summary>
+        private readonly KandaProviderFactory _factory = KandaProviderFactory.Instance;
+
+        #endregion
+    }
+}
---
+++ kkkkkkaaaaaa.kkkkkkaaaaaa.Xunit/Data/TableDataGateways/MembershipsGatewayFacts.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+using kkkkkkaaaaaa.Data.TableDataGateways;
+using kkkkkkaaaaaa.DataTransferObjects;
+using Xunit;
+
+namespace kkkkkkaaaaaa.kkkkkkaaaaaa.Xunit.Data.TableDataGateways
+{
+    public class MembershipsGatewayFacts : KandaTableDataGatewayFacts
+    {
+        public async Task FindAsynkFact()
+        {
+        }
+
+        [Fact()]
+        public async void SelectAsyncFact()
+        {
+
+            var connection = this.Factory.CreateConnection();
+            var transaction = default(DbTransaction);
+            var token = CancellationToken.None;
+
+            try
+            {
+                connection.Open();
+                transaction = connection.BeginTransaction(IsolationLevel.Serializable);
+
+                // 登録
+                var entity = new MembershipEntity()
+                {
+                    ID = 0,
+                    Name = string.Format("MembershipsGatewayFacts.FindAsyncFact(new MembershipEntity {{ Name={0} }}) ", new Random().Next()),
+                    Password = @"password",
+                    Enabled = true,
+                    Email = @"email@example.com",
+                    CreatedOn = DateTime.Now,
+                    // UpdatedOn = CreatedOn,
+                };
+                var inserted = await MembershipsGateway.InsertAsync(entity, connection, transaction, token);
+                Assert.True(0 < inserted);
+
+                // 取得
+                var criteria = new MembershipsCriteria()
+                {
+                    ID = inserted,
+                };
+                var selected = await MembershipsGateway.SelectAsync(criteria, connection, transaction, token);
+                Assert.True(selected.Read());
+                selected.Close();
+
+                // 削除
+                var deleted = await MembershipsGateway.DeleteAsync(inserted, connection, transaction, token);
+                Assert.True(deleted == 1);
+
+                transaction.Commit();
+            }
+            catch
+            {
+                if (transaction != null) { transaction.Rollback(); }
+            }
+            finally
+            {
+                if (connection != null) { connection.Close(); }
+            }
+        }
+    }
+}

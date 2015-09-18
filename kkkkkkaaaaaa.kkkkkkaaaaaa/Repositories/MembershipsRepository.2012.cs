d36705115e9f58f8d25cf7830762ad532b3c4a8d
kkkkkkaaaaaa <kkkkkkaaaaaa@hotmail.co.jp>
2015-09-18 16:37:49 +0900

    Add async methods and tests.

---
+++ kkkkkkaaaaaa.kkkkkkaaaaaa/Repositories/MembershipsRepository.2012.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Common;
+using System.Threading;
+using System.Threading.Tasks;
+using kkkkkkaaaaaa.Data.Common;
+using kkkkkkaaaaaa.Data.TableDataGateways;
+using kkkkkkaaaaaa.DataTransferObjects;
+
+namespace kkkkkkaaaaaa.Repositories
+{
+    public partial class MembershipsRepository
+    {
+        public async Task<MembershipEntity> FindAsync(MembershipsCriteria criteria, DbConnection connection, DbTransaction transaction, CancellationToken token)
+		{
+		    var reader = default(DbDataReader);
+
+		    try
+		    {
+				reader = await MembershipsGateway.SelectAsync(criteria, connection, transaction, token);
+
+				var entity = await reader.ReadAsync()
+                    ? KandaDataMapper.MapToObject<MembershipEntity>(reader)
+                    : MembershipEntity.Empty;
+
+		        return entity;
+		    }
+		    finally
+		    {
+		        if (reader != null) { reader.Close(); }
+		    }
+        }
+    }
+}

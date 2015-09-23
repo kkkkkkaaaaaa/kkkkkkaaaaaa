45c1aa72546aef3a4649e18394489c9df2a37900
kkkkkkaaaaaa <kkkkkkaaaaaa@hotmail.co.jp>
2015-09-23 22:10:18 +0900

    Set routeing.

--- kkkkkkaaaaaa.kkkkkkaaaaaa/Web/Http/WebApiConfig.cs
+++ kkkkkkaaaaaa.kkkkkkaaaaaa/Web/Http/WebApiConfig.cs
@@ -5,21 +5,25 @@ namespace kkkkkkaaaaaa.Web.Http
 {
     public static class WebApiConfig
     {
+        /// <summary>
+        /// URI ルーティングの方法を構成および設定します。
+        /// </summary>
         public static void Register(HttpConfiguration config)
         {
-            // Web API ルート
             // 属性ルーティング
             config.MapHttpAttributeRoutes();
-
 
             // ルートマッピング
             config.Routes.MapHttpRoute(@"Default", @"");
 
+            // アクションメソッドは規約に従う
             config.Routes.MapHttpRoute(
                 name: "WebApi1",
                 routeTemplate: "{controller}/{id:int}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            // アクションメソッドも指定する
             config.Routes.MapHttpRoute(
                 name: "WebApi2",
                 routeTemplate: "{controller}/{action}/{id:int}",

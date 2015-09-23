59d326f3445af50681feac127ab1c68e644f086b
kkkkkkaaaaaa <kkkkkkaaaaaa@hotmail.co.jp>
2015-09-23 22:03:15 +0900

    Add Membership class for experimation.

---
+++ kkkkkkaaaaaa.kkkkkkaaaaaa/Web/Http/WebApiConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Http;
+using System.Web.Routing;
+
+namespace kkkkkkaaaaaa.Web.Http
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API ルート
+            // 属性ルーティング
+            config.MapHttpAttributeRoutes();
+
+
+            // ルートマッピング
+            config.Routes.MapHttpRoute(@"Default", @"");
+
+            config.Routes.MapHttpRoute(
+                name: "WebApi1",
+                routeTemplate: "{controller}/{id:int}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            config.Routes.MapHttpRoute(
+                name: "WebApi2",
+                routeTemplate: "{controller}/{action}/{id:int}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}

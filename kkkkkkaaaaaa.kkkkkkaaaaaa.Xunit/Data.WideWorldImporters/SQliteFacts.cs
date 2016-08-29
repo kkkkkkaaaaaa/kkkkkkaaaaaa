58a529613faebe329cf5909d6c7340b144f4c86d
Kanda, Yuichiro <kkkkkkaaaaaa@hotmail.co.jp>
2016-08-29 10:29:42 +0900

    Add kkkkkkkaaaaaa.2015 projects.

---
+++ kkkkkkaaaaaa.kkkkkkaaaaaa.Xunit/Data.WideWorldImporters/SQliteFacts.cs
@@ -0,0 +1,24 @@
+﻿using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kkkkkkaaaaaa.Data.Common;
+using Xunit;
+
+namespace kkkkkkaaaaaa.Data.WideWorldImporters
+{
+    class SQliteFacts : KandaXunitProviderFactory
+    {
+        [Fact()]
+        public void Fact()
+        {
+            var schema = connection.GetTablesSchema();
+            var reader = new DataTableReader(schema);
+
+            var tables = KandaDbDataMapper.MapToEnumerable(reader);
+
+            //return tables;
+        }
+    }
+}
---
+++ kkkkkkaaaaaa.kkkkkkaaaaaa.Xunit/Data/KandaXunitProviderFactory.2015.cs
@@ -0,0 +1,70 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using kkkkkkaaaaaa.Data.Common;
+using System;
+
+namespace kkkkkkaaaaaa.Data
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class KandaXunitProviderFactory : KandaDbProviderFactory
+    {
+        /// <summary>
+        /// Singleton インスタンス。
+        /// </summary>
+        public static KandaXunitProviderFactory Instance
+        {
+            get { return KandaXunitProviderFactory._instance; }
+        }
+
+
+        /// <summary>
+        /// 接続を生成して返します。
+        /// </summary>
+        /// <returns></returns>
+        public override DbConnection CreateConnection()
+        {
+            var connection = base.CreateConnection();
+            connection.ConnectionString = wwi.ConnectionString;
+
+            return connection;
+        }
+
+        /// <summary>
+        /// コマンドを生成して返します。
+        /// </summary>
+        /// <returns></returns>
+        public override DbCommand CreateCommand()
+        {
+            var command = base.CreateCommand();
+            command.CommandType = CommandType.StoredProcedure;
+
+            return command;
+        }
+
+        #region Protected members...
+
+        /// <summary>
+        /// コンストラクター。
+        /// </summary>
+        protected KandaXunitProviderFactory() : base(DbProviderFactories.GetFactory(_wwi.ProviderName))
+        {
+            this.DoNothing();
+        }
+
+        #endregion
+
+        #region Private members...
+
+        /// <summary>Instance のバッキングフィールド。</summary>
+        private readonly static KandaXunitProviderFactory _instance = new Lazy<KandaXunitProviderFactory>().Value;
+        /// <summary>接続文字列。</summary>
+        private static readonly ConnectionStringSettings _wwi = ConfigurationManager.ConnectionStrings["wwi"];
+
+        #endregion
+
+
+    }
+}

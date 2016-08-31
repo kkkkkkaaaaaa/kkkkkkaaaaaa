9346c2aaefb96e7926f1221b4dff43714db9bc3b
Kanda, Yuichiro <kkkkkkaaaaaa@hotmail.co.jp>
2016-08-31 16:49:54 +0900

    Save.

---
+++ kkkkkkaaaaaa/Windows/Reactive/ButtonExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace kkkkkkaaaaaa.Windows.Reactive
+{
+    public static class ButtonExtensions
+    {
+        /// <summary>
+        /// Click イベント Observable。
+        /// </summary>
+        /// <param name="buttonBase"></param>
+        /// <returns></returns>
+        public static IObservable<EventPattern<RoutedEventArgs>> ClickAsObservable(this ButtonBase buttonBase)
+        {
+            // return Observable.FromEventPattern<RoutedEventHandler, RoutedEventArgs>(
+            //     ((handler) => { return (sender, e) => handler.Invoke(button, e); })
+            //     , ((handler) => { button.Click += handler; })
+            //     , ((handler) => { button.Click -= handler; }));
+            return Observable.FromEventPattern<RoutedEventHandler, RoutedEventArgs>(
+                handler => (sender, e) => handler?.Invoke(buttonBase, e)
+                , handler => buttonBase.Click += handler
+                , handler => buttonBase.Click -= handler);
+        }
+    }
+}
---
+++ kkkkkkaaaaaa/Windows/Reactive/UIElementExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace kkkkkkaaaaaa.Windows.Reactive
+{
+    public static class UiElementExtensions
+    {
+    }
+}

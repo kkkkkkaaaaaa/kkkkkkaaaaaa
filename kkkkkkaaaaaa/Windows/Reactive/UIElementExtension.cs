aef7231678cc3cc23f2fb56d1cc8fb99bcb51c62
Kanda, Yuichiro <kkkkkkaaaaaa@hotmail.co.jp>
2016-09-07 11:11:42 +0900

    Add kkkkkkaaaaaa.Reactive namespace.

--- kkkkkkaaaaaa/Windows/Reactive/UIElementExtension.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace kkkkkkaaaaaa.Windows.Reactive
-{
-    [Obsolete(@"use kkkkkkaaaaaa.Reactive.Windows.UIElementExtensions")]
-    public static class UiElementExtensions
-    {
-    }
-}

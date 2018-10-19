6435303072846d770e489edd863577896efb7754
kkkkkkaaaaaa <kkkkkkaaaaaa@hotmail.co.jp>
2018-10-19 14:54:31 +0900

    Save.

---
+++ kkkkkkaaaaaa/Reactive/Windows/ApplicationExtensiona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Windows;
+
+namespace kkkkkkaaaaaa.Reactive.Windows
+{
+	public static class ApplicationExtensiona
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="app"></param>
+		/// <returns></returns>
+		public static IObservable<StartupEventArgs> StartupAsObservable(this Application app)
+		{
+			return Observable.FromEvent<StartupEventHandler, StartupEventArgs>(
+				(handler) => ((sender, e) => handler.Invoke(e)), 
+				(handler) => app.Startup += handler, 
+				(handler) => app.Startup -= handler);
+		}
+	}
+}
